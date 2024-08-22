4882252cb374cf56aa7927a0a222f1b1d783c307
Aswin Ranjith <aswin.ranjith@iouring.com>
2024-08-22 11:21:17 +0530

    google sso changes. added firebaseClientId in ios config

--- lib/processors/ios/pbxproj/ios_pbxproj_processor.dart
+++ lib/processors/ios/pbxproj/ios_pbxproj_processor.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_flavorizr/parser/models/flavorizr.dart';
 import 'package:flutter_flavorizr/parser/models/flavors/flavor.dart';
 import 'package:flutter_flavorizr/parser/models/flavors/ios/enums.dart';
@@ -68,6 +69,8 @@ class IOSPbxprojProcessor extends StringProcessor {
           );
 
           input = input!.substring(baseConfigPos);
+
+          debugPrint('===>$entryPoint');
 
           buffer.write(
             '$entryPoint = "${getValue(entryPoint, flavor.value)}";',
@@ -178,7 +181,14 @@ class IOSPbxprojProcessor extends StringProcessor {
       case teamIDEntryPoint:
         return flavor.ios.teamID;
       case provProfileEntryPoint:
-        return flavor.ios.profileName;
+        final extensionType = extension == 'ServiceExtension'
+            ? 'Service Extension'
+            : 'Content Extension';
+        if (extension.isNotEmpty) {
+          return '${flavor.ios.profileName} Notification $extensionType';
+        } else {
+          return flavor.ios.profileName;
+        }
       case productBundleIdEntryPoint:
         return '${flavor.ios.bundleId}.$extension';
       default:
